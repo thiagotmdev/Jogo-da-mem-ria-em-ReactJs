ce39798dd7fd2c63656c4fe867196528f30b32e1
Thiago Mendes <thiagomendes.dev@gmail.com>
2022-06-27 18:52:19 -0300

    first commit of repo

---
+++ README.md
@@ -0,0 +1,25 @@
+# Jogo da Memória
+
+Este projeto foi feito em aula no curso da [B7Web](https://b7web.com.br).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
---
+++ src/App.tsx
@@ -0,0 +1,139 @@
+import * as C from './App.styles';
+import logoImage from './assets/devmemory_logo.png'
+import { InfoItem } from './components/InfoItem';
+import { Button } from './components/Button';
+import RestartIcon from './svgs/restart.svg';
+import { useEffect, useState } from 'react';
+import { GridItemType } from './types/GridItemType';
+import { items } from './data/items';
+import { GridItem } from './components/GridItem';
+import { formatTimeElapsed } from './helpers/formatTimeElapsed';
+
+const App = ()=>{
+
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [moveCount, setMoveCount] = useState<number>(0);
+  const [showCount, setShowCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
+
+  useEffect(()=> resetAndCreateGrid(), []);
+
+  useEffect(()=>{
+    const timer = setInterval(()=>{
+      if(playing){
+        setTimeElapsed(timeElapsed + 1);
+      }
+    }, 1000);
+    return()=>clearInterval(timer);
+  }, [playing, timeElapsed]);
+
+  useEffect(()=>{
+    if(showCount === 2){
+      let opened = gridItems.filter(item => item.shown === true);
+      if(opened.length === 2){
+
+        //v1 = if both are equals, make every "shown" permanent
+        if(opened[0].item === opened[1].item){
+          let tmpGrid = [...gridItems];
+          for(let i in tmpGrid){
+            if(tmpGrid[i].shown){
+              tmpGrid[i].permanentShown = true;
+              tmpGrid[i].shown = false;
+            }
+          }
+          setGridItems(tmpGrid);
+          setShowCount(0);
+        }else{
+          //v2 - if they are not equals, close all "shown"
+          setTimeout(()=>{
+            let tmpGrid = [...gridItems];
+            for(let i in tmpGrid){
+              tmpGrid[i].shown = false;
+            }
+            setGridItems(tmpGrid);
+            setShowCount(0);
+          }, 1000);
+        }
+        setMoveCount(moveCount => moveCount + 1);
+      }
+    }
+  }, [showCount, gridItems]); // Essa parte monitora os dados em showCount e gridItems
+
+  //Verify if game is over
+  useEffect(()=>{
+    if(moveCount > 0 && gridItems.every(item=> item.permanentShown === true)){
+      setPlaying(false);
+    }
+  }, [moveCount, gridItems]);
+
+  const resetAndCreateGrid = ()=>{
+    // passo 1 - resetar o jogo
+    setTimeElapsed(0);
+    setMoveCount(0);
+    setShowCount(0);
+
+    // passo 2 - criar o grid e começar o jogo
+
+    // 2.1 - criar um grid vazio
+    let tmpGrid: GridItemType[] = [];
+    for (let i = 0; i < (items.length * 2); i++)tmpGrid.push({item: null, shown:false, permanentShown: false});
+
+    // 2.2 - preencher o grid
+    for(let w =0; w < 2; w++){
+      for(let i =0; i < items.length; i++){
+        let pos = -1;
+        while(pos < 0 || tmpGrid[pos].item !== null){
+          pos = Math.floor(Math.random() *  (items.length * 2));
+        }
+        tmpGrid[pos].item = i;
+      }
+    }
+    // 2.3 - jogar no state
+    setGridItems(tmpGrid);
+
+    // passo 3  - começar o jogo
+    setPlaying(true);
+  }
+
+  const handleItemClick = (index: number)=>{
+    if(playing && index !== null &&showCount < 2){
+      let tmpGrid= [...gridItems];
+      if(tmpGrid[index].permanentShown === false && tmpGrid[index].shown === false){
+        tmpGrid[index].shown = true;
+        setShowCount(showCount + 1);
+      }
+      setGridItems(tmpGrid);
+    }
+  }
+
+  return(
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={logoImage} width="200" alt=""/>
+        </C.LogoLink>
+
+        <C.InfoArea>
+          <InfoItem label='Tempo' value={formatTimeElapsed(timeElapsed)}/>
+          <InfoItem label='Movimentos' value={moveCount.toString()}/>
+        </C.InfoArea>
+
+        <Button label='Reiniciar' icon={RestartIcon} onClick={resetAndCreateGrid}/>
+      </C.Info>
+      <C.GridArea>
+        <C.Grid>
+        {gridItems.map((item, index)=>(
+          <GridItem
+            key = {index}
+            item = {item}
+            onClick={()=> handleItemClick(index)}
+          />
+        ))}
+        </C.Grid>
+      </C.GridArea>
+    </C.Container>
+  );
+}
+
+export default App;
